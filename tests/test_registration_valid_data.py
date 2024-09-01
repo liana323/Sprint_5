0cb6ff74b0a0e9f6a08634e39868b150971929c6
Liana Man <lianabelle323@gmail.com>
2024-09-01 21:15:50 +0400

    Добавление  фикстуры email в test_registration_valid_data

--- tests/test_registration_valid_data.py
+++ tests/test_registration_valid_data.py
@@ -5,13 +5,13 @@ from selenium.webdriver.support import expected_conditions as EC
 
 import locators
 
-def test_registration_valid_data(driver):
+def test_registration_valid_data(driver, email):
 
     driver.get("https://stellarburgers.nomoreparties.site/register")
 
     #Поля ввода для регистрации
     driver.find_element(By.XPATH, locators.name_field).send_keys("Lidia")
-    driver.find_element(By.XPATH, locators.email_field).send_keys("liana_mantashyan_13_987@mail.com")
+    driver.find_element(By.XPATH, locators.email_field).send_keys(email)
     driver.find_element(By.XPATH, locators.password_field).send_keys("45611451")
     driver.find_element(By.XPATH, locators.register_button).click()
 
